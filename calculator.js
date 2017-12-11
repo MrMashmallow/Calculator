9a3ca2cdafb7553083c840213e762bceeeda2c81
Roy Park <roy.park@warriorlife.net>
2017-12-11 11:02:39 -0800

    Errors
    Fixed the javascript sections for errors that casued the calculator to not input numbers.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+function d(val) {
+	document.getElementById("d").value=val;
+}
+function v(val) {
+	document.getElementById("d").value += val;
+}
+
+function e() {
+	try{
+		d(eval(document.getElementById("d").value));
+	}
+	catch(er) {
+		d("Error");
+		
+		
+/*
+// Change Display
+function d(val) {
+	document.getElementById("d").value=val;
+}
+
+//Type numbers and operators
+function v(val) {
+	document.getELementById("d").value+= val;
+}
+
+//Evaluate the equation\
+function e() {
+	try {
+		d ( eval ( document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}*/
