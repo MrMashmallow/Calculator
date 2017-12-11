266b66aefc892222040d1a2ccb364aacd278f86e
Roy Park <roy.park@warriorlife.net>
2017-12-11 11:26:26 -0800

    Fixed Js

--- calculator.js
+++ calculator.js
@@ -13,24 +13,7 @@ function e() {
 	catch(er) {
 		d("Error");
 		
-		
-/*
-// Change Display
-function d(val) {
-	document.getElementById("d").value=val;
-}
-
-//Type numbers and operators
-function v(val) {
-	document.getELementById("d").value+= val;
-}
-
-//Evaluate the equation\
-function e() {
-	try {
-		d ( eval ( document.getElementById("d").value));
 	}
-	catch(err) {
-		d("Error");
-	}
-}*/
+	
+}
+ 
